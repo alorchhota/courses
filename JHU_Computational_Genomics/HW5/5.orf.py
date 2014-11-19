6d29d56b38e2ee37445026915798578464ae6f8c
alorchhota <alorchhota@gmail.com>
2014-11-18 21:35:14 -0500

    CG HW5 initial codes added.

---
+++ JHU_Computational_Genomics/HW5/5.orf.py
@@ -0,0 +1,19 @@
+import sys
+import os
+import re
+import urllib
+import itertools
+
+
+########## settings to work in personal computer ########
+if __name__ != '__main__':
+    workDir = '/User/ashis/Desktop/work/github/courses/JHU_Computational_Genomics/HW5'
+    os.chdir(workDir)
+    sys.path.append(os.path.abspath(os.getcwd()))
+
+################################################################
+############################ part-1#### ########################
+################################################################
+
+
+
